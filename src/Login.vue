34e99bf3022b7ab5bb00fdb11cdd30ef9b53d3b3
jinson <1053438108@qq.com>
2021-07-02 17:41:06 +0800

    add Login.vue

---
+++ src/Login.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Author: JinsonLiang
+ * @Date: 2021-07-02 17:40:22
+ * @LastEditors: JinsonLiang
+ * @LastEditTime: 2021-07-02 17:40:40
+ * @Description: file content
+ * @FilePath: \vue3-vite-test\src\Login.vue
+-->
+<template>
+  <div>==LOGIN PAGE==</div>
+</template>
+
+<script lang='ts'>
+import { ref, defineComponent } from "vue";
+export default defineComponent({
+  name: "App",
+  setup() {
+    const refData = ref(0);
+    return {
+      refData
+    };
+  }
+});
+</script>
+<style scoped>
+</style>
