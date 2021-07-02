567aeac7601e530a4156f985a182bbf85ad3c16d
jinson <1053438108@qq.com>
2021-07-02 17:44:05 +0800

    add home.vue

---
+++ src/Home.vue
@@ -0,0 +1,27 @@
+<!--
+ * @Author: JinsonLiang
+ * @Date: 2021-07-02 17:43:09
+ * @LastEditors: JinsonLiang
+ * @LastEditTime: 2021-07-02 17:43:25
+ * @Description: file content
+ * @FilePath: \vue3-vite-test\src\Home.vue
+-->
+
+<template>
+  <div>==HOME PAGE==</div>
+</template>
+
+<script lang='ts'>
+import { ref, defineComponent } from "vue";
+export default defineComponent({
+  name: "App",
+  setup() {
+    const refData = ref(0);
+    return {
+      refData
+    };
+  }
+});
+</script>
+<style scoped>
+</style>
