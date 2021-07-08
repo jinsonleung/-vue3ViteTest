b0532c2a45671c83da2747ab3938ca319ad6e8dd
JinsonLeung <1053438108@qq.com>
2021-07-08 21:08:56 +0800

    Add global axios

---
+++ src/views/TestAxios.vue
@@ -0,0 +1,34 @@
+<template>
+    
+  <div>==Axios+Mock==</div>
+  <el-form>
+    <el-form-item>
+      <el-button type="primary" @click="getUserInfo">Axios+get请求</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent, getCurrentInstance } from "vue";
+import "../mock/mockServer";
+
+export default defineComponent({
+  name: "TestAxios",
+  setup() {
+    const refData = ref(0);
+    const { proxy }: any = getCurrentInstance();
+    const getUserInfo = () => {
+      console.log(proxy);
+      //使用全局axios方式
+      proxy.$axios.get("/user/info").then((res:any)=>{
+        console.log(res.data);
+      });
+    };
+    return {
+      refData,
+      getUserInfo,
+    };
+  },
+});
+</script>
+<style scoped></style>
